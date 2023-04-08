00e7d00feef830c7c7097c5b5f4d92d35721f9d8
tiger5720 <tiger5720@naver.com>
2023-04-08 11:01:23 +0900

    Decorator 추가

---
+++ src/Decorator/CondimentDecorator.java
@@ -0,0 +1,6 @@
+package Decorator;
+
+public abstract class CondimentDecorator extends Beverage {
+    Beverage beverage;
+    public abstract String getDescription();
+}
---
+++ src/Decorator/DarkRoast.java
@@ -0,0 +1,11 @@
+package Decorator;
+
+public class DarkRoast extends Beverage {
+    public DarkRoast() {
+        description = "Dark Roast Coffee";
+    }
+
+    public double cost() {
+        return .99;
+    }
+}
